60a28e28c270763fdf60492e23261123d521ee02
Balu133 <balajib01052002@gmail.com>
2025-05-31 12:14:21 +0530

    praticing git

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+import com.myapp.Calculator;
+public class MyClass {
+     
+    public static void main(String[] args){
+
+    int a=20,b=10;
+    Calculator calc=new Calculator();
+    System.out.println("Addition="+calc.add(a,b));
+    System.out.println("Substraction="+calc.sub(a,b));
+
+       
+}
+}
